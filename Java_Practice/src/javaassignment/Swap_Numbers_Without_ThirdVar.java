2634267093de56068516cd27c99ca41474cf4a75
Madhukar <madhukar19996@gmail.com>
2025-04-01 07:34:30 +0530

    Adding code Swap_Numbers_Without_ThirdVar

---
+++ Java_Practice/src/javaassignment/Swap_Numbers_Without_ThirdVar.java
@@ -0,0 +1,27 @@
+package javaassignment;
+
+public class Swap_Numbers_Without_ThirdVar {
+
+	public static void main(String[] args) 
+	{
+		// Swap_Two_Numbers_Without_ThirdVariable
+		int a=-1;
+		int b=7;
+		
+		System.out.println("Before Swapping "+" a="+a+ ",b="+b);
+		
+		
+		a=a+b;
+		
+		b=a-b;
+		a=a-b;
+		
+		
+		System.out.println("After Swapping "+" a="+a+",b="+b);
+
+
+		
+
+	}
+
+}
