c39b93a0a148c25aa8a19769c16297400bc86139
Madhukar <madhukar19996@gmail.com>
2025-04-01 08:57:12 +0530

    Adding java code

--- Java_Practice/src/day1/FirstProgram.java
+++ Java_Practice/src/day1/FirstProgram.java
@@ -6,5 +6,6 @@ public class FirstProgram
        {
     	   System.out.println("Welcome to Java");
     	   System.out.println("Namaste India ");
+    	   System.out.println("Namaste Madhukar ");
        }
 }
